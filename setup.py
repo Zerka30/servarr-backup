26b2a4c3f0c4e26e4b770a1b346067d6efb861ea
Zerka30 <contact.zerka@gmail.com>
2024-06-25 11:44:39 +0000

    :package: (package): Fix package

--- setup.py
+++ setup.py
@@ -1,26 +1,4 @@
-from setuptools import setup, find_packages
 
-setup(
-    name="servarr",
-    version="0.1.0",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    entry_points={
-        "console_scripts": [
-            "servarr=app:main",
-        ],
-    },
-    install_requires=[
-        "requests",
-        "humanize",
-        "tabulate",
-        "boto3",
-    ],
-    author="Zerka",
-    author_email="contact@zerka.dev",
-    description="Mediabox Backup Tool",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/Zerka30/mediabox-backup",
-    python_requires=">=3.6",
-)
+import setuptools
+
+setuptools.setup(setup_requires=['pbr>=2.0.0'], pbr=True)
